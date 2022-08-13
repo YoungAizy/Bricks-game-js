3ec3352c0f70b173e5e2284eb8d61535fedbf5c7
Ayanda Marotya <ayandamarotya@gmail.com>
2022-08-13 11:51:24 +0200

    rename webpack entry script

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const htmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     mode: 'production',
     entry: {
-        main: path.resolve(__dirname, 'js/main.js'),
+        main: path.resolve(__dirname, 'js/index.js'),
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
